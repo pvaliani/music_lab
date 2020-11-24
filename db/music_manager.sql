0d8cd2da137376e0c7aa2869e322ad2ff0f2c5ae
Pedram Valiani <p.valiani@gmail.com>
2020-11-24 16:00:29 +0000

    added database tables

---
+++ db/music_manager.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+
+CREATE TABLE artists (
+
+    id SERIAL PRIMARY KEY
+    name VARCHAR(255),
+
+);
+
+
+CREATE TABLE albums (
+    id SERIAL PRIMARY KEY
+    artist_id INT REFERENCES artists(id) -- Foreign Key to artists table
+    title VARCHAR(255),
+    genre VARCHAR(255),
+    
+);
